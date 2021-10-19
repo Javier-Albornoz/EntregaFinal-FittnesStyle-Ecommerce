ac00c31ba9e216b4befec2783e5bad1e9bb05c5f
JavierAlbornoz <javix781@gmail.com>
2021-10-19 16:53:03 -0300

    añadiendo contador

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,3 +1,14 @@
+import { useState } from "react";
+import { ItemsCuont } from "./ItemsCuont";
+
 export const ItemListContainer = ({greeting}) =>{
-    return <h1>{greeting}🏋️‍♂️💪🏋️‍♀️</h1>
+    const [carrito, setCarrito] = useState(1);
+    const onAdd = (cantProductos) =>{
+        setCarrito(cantProductos);
+        alert(`Sumaste ${cantProductos} productos al carrito✨`);
+    } ;
+    return (<>
+    <h1>{greeting}🏋️‍♂️💪🏋️‍♀️</h1>
+    <ItemsCuont disponible={7} inicial={1} onAdd={onAdd} />
+    </>);
 };
---
+++ src/components/ItemsCuont.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import './itemsCuont.scss'
+
+export function ItemsCuont({disponible, inicial, onAdd}){
+    const [counter, setCounter] = useState(inicial);
+    const incrementar = () => {
+        if(counter<disponible){
+            setCounter(counter+1);
+        }
+    }
+    const decrementar = () => {
+        if(counter>inicial){
+            setCounter(counter-1);
+        }
+    }
+    return (
+        <div className = 'counterContainer'>
+            <div className = 'counter'>
+                <div className = 'display'>
+                    <button className = 'btn' onClick={decrementar}>{' '} - {' '}</button>
+                    <div>
+                        <h2>Unidades:{counter}</h2>
+                    </div>
+                    <button className = 'btn' onClick={incrementar}>{' '} + {' '}</button>
+                </div>
+                <button className = 'btn' onClick={()=>onAdd(counter) }>Sumar al carrito</button>
+            </div>
+        </div>
+    )
+}
